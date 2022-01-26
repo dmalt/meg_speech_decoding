a449d98cc0c9ae540e91e41da8e52276b9ce878d
pet67 <33234843+pet67@users.noreply.github.com>
2022-01-26 12:10:19 +0300

    Update run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -1,2 +1,5 @@
 python3 __main__.py --patient procenko --mode regression --model SimpleNetBase_WithLSTM__CNANNELS_6_12__LAG_1000_0__40MELS --runs_count 1 &&\
-python3 __main__.py --patient procenko --mode classification --model SimpleNetBase_WithLSTM__CNANNELS_6_12__LAG_1000_0__40MELS
+python3 __main__.py --patient procenko --mode classification --model SimpleNetBase_WithLSTM__CNANNELS_6_12__LAG_1000_0__40MELS &&\
+python3 __main__.py --patient ivanova --mode regression --model SimpleNetBase_WithLSTM__CNANNELS_ALL__LAG_1000_0__40MELS --runs_count 1 &&\
+python3 __main__.py --patient ivanova --mode classification --model SimpleNetBase_WithLSTM__CNANNELS_ALL__LAG_1000_0__40MELS
+
