7cddb4bc21fe87e14759d297381e7a54a97238d5
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-05-11 09:19:53 +0300

    Remove obsolete launchers & requrements

--- run_batch.sh
+++
@@ -1,2 +0,0 @@
-python3 __main__.py --patient procenko --mode regression --model DenseNet_WithLSTM__CNANNELS_6_12__LAG_1000_0__40MELS --runs_count 2 &&\
-# python3 __main__.py --patient procenko --mode classification --model DenseNet_WithLSTM__CNANNELS_6_12__LAG_1000_0__40MELS
