5283a0bfe2866cf3ccf351848a607f03513b078f
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-05-31 19:40:18 +0300

    Add array types for signal and batch

--- library/type_aliases.py
+++ library/type_aliases.py
@@ -5,3 +5,9 @@ import numpy.typing as npt
 
 Array = npt.NDArray[Any]
 Array32 = npt.NDArray[np.float32]
+
+SignalArray = npt.NDArray[Any]  # array of shape (n_samples, n_sensors)
+SignalArray32 = npt.NDArray[np.float32]  # array of shape (n_samples, n_sensors)
+
+BatchSignalArray = npt.NDArray[Any]  # array of shape (batch_size, n_samples, n_sensors)
+BatchSignalArray32 = npt.NDArray[np.float32]  # array of shape (batch_size, n_samples, n_sensors)
