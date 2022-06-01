10f8ee77bf33726e3760f4af2894870ca4109b64
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-06-01 12:36:03 +0300

    Tinker with config and run_interpreter

--- run_interpreter.py
+++ run_interpreter.py
@@ -20,10 +20,10 @@ setup_utils.setup_hydra()
 
 # MODEL_DATE = "2022-05-09"
 # MODEL_TIME = "13-47-04"
-MODEL_DATE = "2022-05-19"
+MODEL_DATE = "2022-06-01"
 # MODEL_TIME = "18-42-04"
 # MODEL_TIME = "23-17-30"
-MODEL_TIME = "23-21-29"
+MODEL_TIME = "01-28-00"
 OUTPUTS_DIR = Path(f"./outputs/{MODEL_DATE}/{MODEL_TIME}/")
 CONFIG_PATH = OUTPUTS_DIR / ".hydra"
 MODEL_PATH = OUTPUTS_DIR / "model_dumps/BenchModelRegressionBase.pth"
