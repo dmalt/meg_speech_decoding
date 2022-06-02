5af5261de6f553fe2d237dc7afe9cd28cbd31562
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-06-02 19:48:38 +0300

    Refactor annotations

---
+++ library/io/annotations.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+from typing import NamedTuple, Sequence
+
+from mne.io import BaseRaw  # type: ignore
+
+
+class Annotation(NamedTuple):
+    onset: float
+    duration: float
+    type: str
+
+
+def from_raw(raw: BaseRaw) -> list[Annotation]:
+    onsets: list[float] = list(raw.annotations.onset)
+    durations: list[float] = list(raw.annotations.duration)
+    types: list[str] = list(raw.annotations.description)
+    onsets = [o - raw.first_samp / raw.info["sfreq"] for o in raw.annotations.onset]
+    return [Annotation(o, d, t) for o, d, t, in zip(onsets, durations, types)]
+
+
+def get_good_slices(annotations: Sequence[Annotation], sr: float) -> list[slice]:
+    """Get slices for periods with types not starting with 'BAD'"""
+    events = []
+    # when start and end occur simoultaneosly, first add new bad segment, then remove old
+    START = 0
+    END = 1
+    # mypy doesnt believe __getitem__ makes it iterable
+    for onset, duration, _ in filter(lambda a: a.type.startswith("BAD"), annotations):
+        events.append((int(onset * sr), START))
+        events.append((int((onset + duration) * sr), END))
+    events.sort()
+    bad_overlap_cnt = 0
+
+    res = []
+    last_start = 0
+    for ev in events:
+        if ev[1] == START:
+            if bad_overlap_cnt == 0:
+                res.append(slice(last_start, ev[0]))
+            bad_overlap_cnt += 1
+        elif ev[1] == END:
+            bad_overlap_cnt -= 1
+            if bad_overlap_cnt == 0:
+                last_start = ev[0]
+    # process last event
+    if bad_overlap_cnt == 0:
+        res.append(slice(last_start, None))
+    return res
