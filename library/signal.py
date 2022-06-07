2468e4276da1fbc8142094fdb8c008b1f85b4cdd
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-06-07 11:57:45 +0300

    Implement dropping bad segments

--- library/signal.py
+++ library/signal.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
-from typing import Generic, List, NamedTuple, TypeVar
+from typing import Generic, List, NamedTuple, Optional, TypeVar
 
 import numpy as np
 import numpy.typing as npt
@@ -33,7 +33,7 @@ class Signal(Generic[T]):
 
     """
 
-    data: npt.NDArray["np.floating[T]"]
+    data: npt.NDArray[np.floating[T]]
     sr: float
     annotations: Annotations = field(default_factory=list)
 
@@ -44,12 +44,13 @@ class Signal(Generic[T]):
         """Signal length in samples"""
         return self.n_samples
 
-    def __array__(self, dtype=None) -> npt.NDArray["np.floating[T]"]:
+    def __array__(self, dtype=None) -> npt.NDArray[np.floating[T]]:
         if dtype is not None:
             return self.data.astype(dtype)
         return self.data
 
-    def update(self, data: npt.NDArray["np.floating[T]"]) -> Signal[T]:
+    def update(self, data: npt.NDArray[np.floating[T]]) -> Signal[T]:
+        assert len(data) == len(self), f"Can only update with equal length data; got {len(data)=}"
         return self.__class__(data, self.sr, self.annotations)
 
     @property
@@ -66,44 +67,82 @@ class Signal(Generic[T]):
 
 
 def drop_bad_segments(signal: Signal[T]) -> Signal[T]:
-    good_slices = get_good_slices(signal.annotations, signal.sr)
-    good_data: npt.NDArray[np.floating[T]] = np.concatenate([signal.data[s] for s in good_slices])
-    continuous_annotations: list[Annotation] = []
-    prev_time = 0.0
-    for s in good_slices:
-        onset = prev_time
-        stop_samp = len(signal) if s.stop is None else s.stop
-        duration = (stop_samp - s.start) / signal.sr
-        continuous_annotations.append(Annotation(onset, duration, type="CONTINUOUS"))
-        prev_time = duration
+    normalized_annots = AnnotationsOverlapProcessor(signal.annotations).normalize_bad_segments()
+    # good_slices = _get_good_slices(signal.annotations)
+    removed_dur = 0.0
+    new_annots: Annotations = []
+    good_segments_mask = np.ones(len(signal), dtype=bool)
 
-    return Signal(good_data, signal.sr, continuous_annotations)
+    for a in normalized_annots:
+        if not a.type.startswith("BAD"):
+            new_annots.append(Annotation(a.onset - removed_dur, a.duration, a.type))
+        else:
+            segment_start_samp = int(a.onset * signal.sr)
+            segment_end_samp = int((a.onset + a.duration) * signal.sr)
+            good_segments_mask[segment_start_samp:segment_end_samp] = False
+            new_annots.append(Annotation(a.onset - removed_dur, duration=0, type="BAD_BOUNDARY"))
+            removed_dur += a.duration
+
+    return Signal(signal.data[good_segments_mask], signal.sr, new_annots)
 
 
-def get_good_slices(annotations: Annotations, sr: float) -> list[slice]:
-    """Get slices for periods with types not starting with 'BAD'"""
-    events = []
-    # when start and end occur simoultaneosly, first add new bad segment, then remove old
+class AnnotationsOverlapProcessor:
+    # order important: when segment start and end occur simoultaneosly, first
+    # add new bad segment, then remove old
     START = 0
     END = 1
-    for onset, duration, _ in filter(lambda a: a.type.startswith("BAD"), annotations):
-        events.append((int(onset * sr), START))
-        events.append((int((onset + duration) * sr), END))
-    events.sort()
-    bad_overlap_cnt = 0
 
-    res = []
-    last_start = 0
-    for ev in events:
-        if ev[1] == START:
-            if bad_overlap_cnt == 0:
-                res.append(slice(last_start, ev[0]))
-            bad_overlap_cnt += 1
-        elif ev[1] == END:
-            bad_overlap_cnt -= 1
-            if bad_overlap_cnt == 0:
-                last_start = ev[0]
-    # process last event
-    if bad_overlap_cnt == 0:
-        res.append(slice(last_start, None))
-    return res
+    def __init__(self, annotations: Annotations):
+        self.events = []
+        for a in annotations:
+            self.events.append((a.onset, self.START, a))
+            self.events.append((a.onset + a.duration, self.END, a))
+        self.events.sort()
+        self.bad_overlap_cnt = 0
+        self.res: Annotations = []
+        self.open_segments: dict[Annotation, float] = {}
+        self.bad_start: Optional[float] = None
+        self._process_all()
+
+    def normalize_bad_segments(self) -> Annotations:
+        return self.res
+
+    def _process_all(self) -> None:
+        for ev_onset, ev_type, ev_annotation in self.events:
+            self._process_event(ev_onset, ev_type, ev_annotation)
+        assert not self.open_segments
+
+    def _process_event(self, ev_onset: float, ev_type, ev_annotation: Annotation) -> None:
+        is_bad = ev_annotation.type.startswith("BAD")
+        if ev_type == self.START and is_bad:
+            if self.bad_overlap_cnt == 0:
+                self._finalize_all_open_segments(end=ev_onset)
+                self.bad_start = ev_onset
+            self.bad_overlap_cnt += 1
+        elif ev_type == self.START and not is_bad:
+            self._start_tracking_good_segment(ev_annotation)
+        elif ev_type == self.END and is_bad:
+            self.bad_overlap_cnt -= 1
+            if self.bad_overlap_cnt == 0:
+                self._change_tracked_segments_onset(new_onset=ev_onset)
+                assert self.bad_start is not None
+                self.res.append(Annotation(self.bad_start, ev_onset - self.bad_start, "BAD"))
+        else:  # end and good
+            self._stop_tracking_good_segment(ev_onset, ev_annotation)
+
+    def _start_tracking_good_segment(self, annotation):
+        self.open_segments[annotation] = annotation.onset
+
+    def _change_tracked_segments_onset(self, new_onset):
+        self.open_segments.update((a, new_onset) for a in self.open_segments)
+
+    def _finalize_all_open_segments(self, end):
+        self.res.extend(Annotation(o, end - o, a.type) for a, o in self.open_segments.items())
+
+    def _stop_tracking_good_segment(self, ev_onset, annotation):
+        annot_onset = self.open_segments.pop(annotation)
+        if self.bad_overlap_cnt == 0:
+            self._finalize_segment(ev_onset, annot_onset, annotation)
+
+    def _finalize_segment(self, ev_onset, annot_onset, annot):
+        self.res.append(Annotation(annot_onset, ev_onset - annot_onset, annot.type))
--- library/tests/test_signal.py
+++ library/tests/test_signal.py
@@ -1,12 +1,11 @@
 import numpy as np
 import pytest
 
-from library.signal import Annotation, Signal, drop_bad_segments, get_good_slices
+from library.signal import Annotation, Signal, drop_bad_segments
 
-
-def test_get_good_slices():
-    chunks = get_good_slices([Annotation(10, 5, "BAD"), Annotation(11, 5, "BAD")], sr=1)
-    assert chunks == [slice(0, 10), slice(16, None)]
+# def test_get_good_slices():
+#     chunks = _get_good_slices([Annotation(10, 5, "BAD"), Annotation(11, 5, "BAD")], sr=1)
+#     assert chunks == [slice(0, 10), slice(16, None)]
 
 
 @pytest.fixture(params=[np.float128, np.float64, np.float32, np.float16])
@@ -21,9 +20,22 @@ def test_drop_bad_segments_preserves_signal_dtype(signal: Signal) -> None:
 
 
 def test_drop_bad_segments_creates_proper_annotations(signal: Signal) -> None:
-    signal.annotations = [Annotation(0.2, 2, "BAD")]
-    wo_bads_annots = drop_bad_segments(signal).annotations
-    assert len(wo_bads_annots) == 2
-    assert all(annot.type == "CONTINUOUS" for annot in wo_bads_annots)
-    assert wo_bads_annots[0] == Annotation(0, 0.2, "CONTINUOUS")
-    assert wo_bads_annots[1] == Annotation(0.2, len(signal) / signal.sr - 2.2, "CONTINUOUS")
+    signal.annotations = [
+        Annotation(0.2, 2, "BAD"),
+        Annotation(1.2, 2, "BAD"),
+        Annotation(4, 1, "BAD"),
+        Annotation(0.1, 5, "GOOD"),
+    ]
+    wo_bads = drop_bad_segments(signal)
+    wo_bads.annotations = [
+        Annotation(round(a.onset, 4), round(a.duration, 4), a.type) for a in wo_bads.annotations
+    ]
+    # assert len(wo_bads.annotations) == 2
+    assert Annotation(0.2, 0, "BAD_BOUNDARY") in wo_bads.annotations
+    assert Annotation(1, 0, "BAD_BOUNDARY") in wo_bads.annotations
+    assert Annotation(0.1, 0.1, "GOOD") in wo_bads.annotations
+    assert Annotation(0.2, 0.8, "GOOD") in wo_bads.annotations
+    assert Annotation(1, 0.1, "GOOD") in wo_bads.annotations
+    assert len(wo_bads) == len(signal) - 4 * signal.sr
+    # assert wo_bads_annots[0] == Annotation(0, 0.2, "CONTINUOUS")
+    # assert wo_bads_annots[1] == Annotation(0.2, len(signal) / signal.sr - 2.2, "CONTINUOUS")
