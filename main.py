0ddb3882283e6b93146497a542c2ac93fadf5270
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-06-08 11:38:38 +0300

    remove classification branch from main

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import os.path
 from time import perf_counter
 
 import hydra
-import setup_utils
 from hydra.utils import instantiate
-from library.bench_models_regression import BenchModelRegressionBase
-from library.runner_classification import run_classification
-from library.runner_regression import run_regression
 from omegaconf import OmegaConf
+
+import setup_utils
+from library.bench_models_regression import BenchModelRegressionBase
+from library.runner_regression import run_regression
 
 log = logging.getLogger(__name__)
 
@@ -34,12 +34,9 @@ def main(cfg):
     t2 = perf_counter()
     log.info(f"Loading data finished in {t2 - t1:.2f} sec.")
 
-    if "regression" in cfg.model:
-        model = instantiate(cfg.model.regression)
-        bench_model = BenchModelRegressionBase(model, cfg.model.learning_rate)
-        run_regression(bench_model, dataset, cfg.runner, detect_voice)
-    elif cfg.model == "classification":
-        run_classification(cfg.pipeline.model, cfg.patient, cfg.debug)
+    model = instantiate(cfg.model.regression)
+    bench_model = BenchModelRegressionBase(model, cfg.model.learning_rate)
+    run_regression(bench_model, dataset, cfg.runner, detect_voice)
 
     t3 = perf_counter()
     log.info(f"Model training finished in {t3 - t2:.2f} sec")
