7405431e06880b42f0851dc6ea3e1ee8568afcc9
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-06-10 17:29:35 +0300

    Add docstrings, improve type hints

--- library/datasets/speech_meg.py
+++ library/datasets/speech_meg.py
@@ -7,45 +7,50 @@ from typing import Optional
 import librosa as lb  # type: ignore
 import mne  # type: ignore
 import numpy as np
+import numpy.typing as npt
 from mne.io import BaseRaw  # type: ignore
 
-from ..signal import Annotation, Annotations
-from ..type_aliases import Signal32
+from ..signal import Annotation, Annotations, Signal, Signal1D
 
 log = logging.getLogger(__name__)
 
 
 @dataclass
 class Info:
+    """Meg speech dataset info"""
+
     mne_info: mne.Info
 
 
 @dataclass
 class Subject:
     """MEG subject configuration"""
+
     raw_path: str
     audio_path: str
     annotations_path: Optional[str]
 
 
-def read(subject: Subject) -> tuple[Signal32, Signal32, Info, Annotations]:
-    X, info, annotations = _read_raw(subject.raw_path, subject.annotations_path)
+def read(subject: Subject) -> tuple[Signal[npt._32Bit], Signal1D[npt._32Bit], Info]:
+    X, info = _read_raw(subject.raw_path, subject.annotations_path)
     Y = _read_wav(subject.audio_path)
-    return X, Y, info, annotations
+    Y.annotations = X.annotations
+    assert abs(X.duration - Y.duration) < 0.01, "inconsistent durations for audio and MEG"
+    return X, Y, info
 
 
-def _read_wav(path: str, sr=None) -> Signal32:
-    data, sr = lb.load(path, sr=sr)  # pyright: ignore
-    return Signal32(data[:, np.newaxis], sr)
+def _read_wav(path: str, sr: int | None = None) -> Signal1D[npt._32Bit]:
+    data, sr_final = lb.load(path, sr=sr)  # pyright: ignore
+    return Signal1D(data[:, np.newaxis], sr_final, [])
 
 
-def _read_raw(raw_path: str, annot_path: Optional[str]) -> tuple[Signal32, Info, Annotations]:
+def _read_raw(raw_path: str, annot_path: str | None) -> tuple[Signal[npt._32Bit], Info]:
     raw = mne.io.read_raw_fif(raw_path, verbose="ERROR", preload=True)
     if annot_path is not None:
         annots = mne.read_annotations(annot_path)
         raw.set_annotations(annots)
     X_data = raw.get_data(picks="meg").astype("float32").T
-    return Signal32(X_data, raw.info["sfreq"]), Info(raw.info), _annotations_from_raw(raw)
+    return Signal(X_data, raw.info["sfreq"], _annotations_from_raw(raw)), Info(raw.info)
 
 
 def _annotations_from_raw(raw: BaseRaw) -> Annotations:
